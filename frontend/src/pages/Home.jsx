221b4ee0caeee5d751164874c642b6ef4168fb68
Abdikhafar Issack <abdikhafarissack@gmail.com>
2025-02-17 16:56:53 +0300

    mobile responsiveness

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,35 @@
-import { Box, Flex, Text,Image } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 import Crousel from "../components/Home/Crousel";
 import AboutUs from "../components/Home/AboutUs";
 import Trending from "../components/Home/Trending";
 import CardSlide from "./Products";
 import Middle from "../components/Home/Testimonials";
-import Contact from "../components/Home/Contact"
+import Contact from "../components/Home/Contact";
 
 export default function Home() {
   return (
-    <Flex justifyContent={"center"} flexDir={"column"} alignItems={"center"}>
+    <Flex justifyContent="center" flexDir="column" alignItems="center">
       <Box
-        bg={"black"}
-        w={"92%"}
-        h={"40px"}
-        color={"white"}
-        textAlign={"center"}
-        py="8px"
-        fontSize={"12px"}
+        bg="black"
+        w="92%"
+        minH="40px"
+        color="white"
+        textAlign="center"
+        py={{ base: "6px", md: "8px" }}
+        fontSize={{ base: "12px", md: "14px" }}
         m={3}
         _hover={{ bg: "white", color: "black" }}
       >
-        {" "}
         <Text 
-  fontSize={{ base: "14px", md: "18px" }} 
-  fontWeight="bold"
-  textAlign="center"
-  px={{ base: 2, md: 0 }} 
->
-  Get 15% off select products, plus 2 free gifts on orders over KSh 10,000!
-</Text>
-
+          fontSize={{ base: "12px", sm: "14px", md: "16px" }} 
+          fontWeight="bold"
+          textAlign="center"
+          px={{ base: 2, md: 4 }}
+        >
+          Get 15% off select products, plus 2 free gifts on orders over KSh 10,000!
+        </Text>
       </Box>
+
       <Crousel />
       <AboutUs />
       <Trending />
@@ -40,5 +39,3 @@ export default function Home() {
     </Flex>
   );
 }
-
-
